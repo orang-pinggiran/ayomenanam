7ecd11e07b5c265a933f3f3b2fe83ceb93791b79
yesikatr <trestiarso@gmail.com>
2019-05-12 13:28:00 +0700

    detail adopsi

--- application/views/pengguna/detail_adopsi.php
+++ application/views/pengguna/detail_adopsi.php
@@ -1,166 +1,64 @@
-							<?php echo $this->session->flashdata('info'); ?>
 
-                 <!-- Horizontal Layout -->
-                       
+<!------ Include the above in your HEAD tag ---------->
 
-							<div class="row" align="center">
-								<div class="col-xs-6 col-sm-6">
-									
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+ <div class="row">
+					<?php 
+                    $poster = $this->db->query("Select * from tbl_event where id_event='$id_event'");
+                    foreach ($poster->result() as $row1 ) {       
+                    ?>
+                    <div class="col-md-6 product_img">
+                        <img src="<?php echo base_url('adminBSB/images/'.$row1->poster); ?>" class="img-responsive">
+                    </div>
+                    <div class="col-md-6 product_content">
+						<h5>Detail Event</h5>
+						<hr>
+						<p><i class="fa fa-asterisk"></i> Nama event <?php echo $row1->judul_event ?></p>
+                         <p><i class="fa fa-thumb-tack"></i> Lokasi <?php echo $row1->tempat_event; ?></p>
+                         <p><i class="fa fa-calendar"></i> Tanggal <?php echo parse_time($row1->tanggal_event,'d F Y'); ?>,
+						 pukul <?php echo parse_time($row1->waktu_event,'H:i') ; ?> WIB</p>
+						<?php }?>
+						<hr>
+						<h5>Detail Adopsi</h5>
+						<hr>
+						
+						<?php
+						$pohon = $this->db->query('Select * from jenis_pohon WHERE id_jenis_pohon='.$id_jenis_pohon.' ');
+						foreach ($pohon->result() as $row2) {
+						?>
+                         <p><i class="glyphicon glyphicon-shopping-cart"></i> Jenis adopsi <?php echo strtolower($jenis_adopsi); ?> </p>
+                        <?php
+						$posko = $this->db->query('Select * from tbl_posko WHERE id_posko='.$id_posko.' ');
+						foreach ($posko->result() as $row4) {
+						?>
+						 <p><i class="fa fa-university"></i> Ambil dari posko <?php echo $row4->nama_posko; ?> </p>
+						<?php } ?>
+						 <p><i class="fa fa-tree"></i> Nama pohon <b><?php echo $nama_pohon; ?> </b> jenis pohon <?php echo strtolower($row2->nama_jenis_pohon); ?> dengan jumlah <?php echo $jumlah_pohon; ?></p>
+						<?php }?>
+                        <p><i class="fa fa-calendar"></i> Tanggal adopsi <?php echo parse_time($tgl_adopsi,'d F Y'); ?>, 
+						pukul <?php echo parse_time($waktu_adopsi,'H:i') ; ?> WIB</p>
+						<p><i class="fa fa-chevron-right"></i> Status <span class="label bg-light-blue"><?php echo strtolower($status_adopsi); ?> </span> </p>
+
+                        <hr>
+                        <div class="space-ten"></div>
+
+					<div class="header">
+                            <h5>
+                                Lokasi Event
+                            </h5>
+                        </div>
+					</div>
+				      <div class="col-md-12 product_img">
+						<div class="form-group">
+                            <div id="map" style="width:100%; height:300px; border:1px solid green;"></div>
+							<?php 
+								$lokasi = $this->db->query("Select * from tbl_event where id_event='$id_event'");
+								foreach ($lokasi->result() as $row3 ) {       
+								?>
+							<input type="hidden" name="latitude_event" id="latitude_event" value="<?php echo $row3->latitude_event; ?>">
+							<input type="hidden" name="longitude_event" id="longitude_event" value="<?php echo $row3->longitude_event; ?>">
+                            </div>
+								<?php } ?>
 								</div>
-								<div class="col-xs-8 col-sm-12">
-									
-									<div class="row clearfix">
-                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
-                                        <label>Jenis Adopsi</label>
-                                    </div>
-                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-                                        <div class="form-group">
-									<div class="form-line disabled">                                                
-										 <input type="text" class="form-control" value="<?php echo $jenis_adopsi;?>" disabled />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>	
-									
-									<div class="row clearfix">
-                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
-                                        <label>Nama posko</label>
-                                    </div>
-									<?php 
-                                                    $posko = $this->db->query("Select * from tbl_posko where id_posko='$id_posko'");
-                                                    foreach ($posko->result() as $row1 ) {
-                                                       
-                                                ?>
-                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-                                        <div class="form-group">
-									<div class="form-line disabled">                                                
-										 <input type="text" class="form-control" value="<?php echo $row1->nama_posko;?>" disabled />
-                                            </div>
-											 <?php }?>
-                                        </div>
-                                    </div>
-                                </div>
-								
-								<div class="row clearfix">
-                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
-                                        <label>Jenis pohon</label>
-                                    </div>
-									<?php 
-                                                    $jenis_pohon = $this->db->query("Select * from jenis_pohon where id_jenis_pohon='$id_jenis_pohon'");
-                                                    foreach ($jenis_pohon->result() as $row2 ) {
-                                                       
-                                                ?>
-                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-                                        <div class="form-group">
-									<div class="form-line disabled">                                                
-										 <input type="text" class="form-control" value="<?php echo $row2->nama_jenis_pohon;?>" disabled />
-                                            </div>
-											<?php }?>
-                                        </div>
-                                    </div>
-                                </div>
-									
-								<div class="row clearfix">
-                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
-                                        <label>Jumlah pohon</label>
-                                    </div>
-                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-                                        <div class="form-group">
-									<div class="form-line disabled">                                                
-										 <input type="text" class="form-control" value="<?php echo $jumlah_pohon;?>" disabled />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>	
-
-								<div class="row clearfix">
-                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
-                                        <label>Tanggal adopsi</label>
-                                    </div>
-									<?php function format_indo1($date){
-									$BulanIndo = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
-
-									$tahun = substr($date, 0, 4);               
-									$bulan = substr($date, 5, 2);
-									$tgl   = substr($date, 8, 2);
-									$result = $tgl . "&nbsp;" . $BulanIndo[(int)$bulan-1]. "&nbsp;". $tahun;
-									return($result);
-									}?>
-                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-                                        <div class="form-group">
-									<div class="form-line disabled">                                                
-										 <input type="text" class="form-control" value="<?php echo format_indo1($tgl_adopsi); ?>" disabled />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-								
-								<div class="row clearfix">
-                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
-                                        <label>Waktu adopsi</label>
-                                    </div>
-                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-                                        <div class="form-group">
-									<div class="form-line disabled">                                                
-										 <input type="text" class="form-control" value="<?php echo $waktu_adopsi;?>" disabled />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-								
-								<div class="row clearfix">
-                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
-                                        <label>Status</label>
-                                    </div>
-                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-                                        <div class="form-group">
-									<div class="form-line disabled">                                                
-										 <input type="text" class="form-control" value="<?php echo $status_adopsi;?>" disabled />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-								
-								<div class="row clearfix">
-                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
-                                        <label>Keterangan</label>
-                                    </div>
-                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-                                        <div class="form-group">
-									<div class="form-line disabled">                                                
-										  <textarea rows="4" class="form-control no-resize" disabled><?php echo $keterangan;?></textarea>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-									
-								</div>
-								 
-							</div>
-							<table border="0" width="700">
-							<tr>
-							<td align="right">
-                                        <a href="<?php echo base_url();?>pengguna/adopsi" class="btn bg-orange m-t-15 waves-effect">Kembali </button></a>
-								</td>
-								</tr>
-								</table>
-					
-            <!-- #END# Horizontal Layout -->
-
-    
-
-
-
-    <!-- Jquery DataTable Plugin Js -->
-    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/jquery.dataTables.js"></script>
-    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
-    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
-    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
-    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
-    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
-    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
-    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
-    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
-
-    <!-- Custom Js -->
-    <script src="<?php echo base_url(); ?>adminBSB/js/pages/tables/jquery-datatable.js"></script>
+		<script src="<?php echo base_url(); ?>adminBSB/js/maap.js"></script>
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD6MLwjbd-cuOCFqZ48OWjmWGsyoZTlIag&libraries=places&callback=initAutocomplete" async defer></script>
---
+++ application/views/pengguna/detailadopsi.php
@@ -0,0 +1,166 @@
+							<?php echo $this->session->flashdata('info'); ?>
+
+                 <!-- Horizontal Layout -->
+                       
+
+							<div class="row" align="center">
+								<div class="col-xs-6 col-sm-6">
+									
+								</div>
+								<div class="col-xs-8 col-sm-12">
+									
+									<div class="row clearfix">
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                        <label>Jenis Adopsi</label>
+                                    </div>
+                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+                                        <div class="form-group">
+									<div class="form-line disabled">                                                
+										 <input type="text" class="form-control" value="<?php echo $jenis_adopsi;?>" disabled />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>	
+									
+									<div class="row clearfix">
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                        <label>Nama posko</label>
+                                    </div>
+									<?php 
+                                                    $posko = $this->db->query("Select * from tbl_posko where id_posko='$id_posko'");
+                                                    foreach ($posko->result() as $row1 ) {
+                                                       
+                                                ?>
+                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+                                        <div class="form-group">
+									<div class="form-line disabled">                                                
+										 <input type="text" class="form-control" value="<?php echo $row1->nama_posko;?>" disabled />
+                                            </div>
+											 <?php }?>
+                                        </div>
+                                    </div>
+                                </div>
+								
+								<div class="row clearfix">
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                        <label>Jenis pohon</label>
+                                    </div>
+									<?php 
+                                                    $jenis_pohon = $this->db->query("Select * from jenis_pohon where id_jenis_pohon='$id_jenis_pohon'");
+                                                    foreach ($jenis_pohon->result() as $row2 ) {
+                                                       
+                                                ?>
+                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+                                        <div class="form-group">
+									<div class="form-line disabled">                                                
+										 <input type="text" class="form-control" value="<?php echo $row2->nama_jenis_pohon;?>" disabled />
+                                            </div>
+											<?php }?>
+                                        </div>
+                                    </div>
+                                </div>
+									
+								<div class="row clearfix">
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                        <label>Jumlah pohon</label>
+                                    </div>
+                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+                                        <div class="form-group">
+									<div class="form-line disabled">                                                
+										 <input type="text" class="form-control" value="<?php echo $jumlah_pohon;?>" disabled />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>	
+
+								<div class="row clearfix">
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                        <label>Tanggal adopsi</label>
+                                    </div>
+									<?php function format_indo1($date){
+									$BulanIndo = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
+
+									$tahun = substr($date, 0, 4);               
+									$bulan = substr($date, 5, 2);
+									$tgl   = substr($date, 8, 2);
+									$result = $tgl . "&nbsp;" . $BulanIndo[(int)$bulan-1]. "&nbsp;". $tahun;
+									return($result);
+									}?>
+                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+                                        <div class="form-group">
+									<div class="form-line disabled">                                                
+										 <input type="text" class="form-control" value="<?php echo format_indo1($tgl_adopsi); ?>" disabled />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+								
+								<div class="row clearfix">
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                        <label>Waktu adopsi</label>
+                                    </div>
+                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+                                        <div class="form-group">
+									<div class="form-line disabled">                                                
+										 <input type="text" class="form-control" value="<?php echo $waktu_adopsi;?>" disabled />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+								
+								<div class="row clearfix">
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                        <label>Status</label>
+                                    </div>
+                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+                                        <div class="form-group">
+									<div class="form-line disabled">                                                
+										 <input type="text" class="form-control" value="<?php echo $status_adopsi;?>" disabled />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+								
+								<div class="row clearfix">
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                        <label>Keterangan</label>
+                                    </div>
+                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+                                        <div class="form-group">
+									<div class="form-line disabled">                                                
+										  <textarea rows="4" class="form-control no-resize" disabled><?php echo $keterangan;?></textarea>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+									
+								</div>
+								 
+							</div>
+							<table border="0" width="700">
+							<tr>
+							<td align="right">
+                                        <a href="<?php echo base_url();?>pengguna/adopsi" class="btn bg-orange m-t-15 waves-effect">Kembali </button></a>
+								</td>
+								</tr>
+								</table>
+					
+            <!-- #END# Horizontal Layout -->
+
+    
+
+
+
+    <!-- Jquery DataTable Plugin Js -->
+    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/jquery.dataTables.js"></script>
+    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
+    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+    <script src="<?php echo base_url(); ?>adminBSB/plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
+
+    <!-- Custom Js -->
+    <script src="<?php echo base_url(); ?>adminBSB/js/pages/tables/jquery-datatable.js"></script>
