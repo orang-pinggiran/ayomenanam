20f484649872e4588b6b78ee259d0dbec44deb2d
yesikatr <trestiarso@gmail.com>
2019-05-12 20:00:19 +0700

    total posko, pengguna, komunitas

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,51 +1,51 @@
 <div class="container-fluid">
-    <div class="block-header">
-        <h2>DASHBOARD</h2>
-    </div>
-
     <!-- Widgets -->
     <div class="row clearfix">
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
             <div class="info-box bg-pink hover-expand-effect">
                 <div class="icon">
-                    <i class="material-icons">playlist_add_check</i>
+                    <i class="material-icons">people</i>
                 </div>
                 <div class="content">
-                    <div class="text">NEW TASKS</div>
-                    <div class="number count-to" data-from="0" data-to="125" data-speed="15" data-fresh-interval="20">125</div>
-                </div>
+                    <div class="text">KOMUNITAS</div>
+					<?php
+						$cek = $this->db->query('Select count(id_pengguna) AS jumlah from tbl_pengguna where level="2" ');
+						foreach ($cek->result() as $row) {
+								?>
+                    <div class="number count-to" data-from="0" data-to="<?php echo $row->jumlah; ?>" data-speed="1000" data-fresh-interval="20"><?php echo $row->jumlah; ?></div>
+						<?php } ?>
+				</div>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
             <div class="info-box bg-cyan hover-expand-effect">
                 <div class="icon">
-                    <i class="material-icons">help</i>
+                    <i class="material-icons">account_balance</i>
                 </div>
                 <div class="content">
-                    <div class="text">NEW TICKETS</div>
-                    <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">257</div>
+                    <div class="text">POSKO</div>
+					<?php
+						$cek2 = $this->db->query('Select count(id_pengguna) AS jumlah2 from tbl_pengguna where level="4" ');
+						foreach ($cek2->result() as $row2) {
+								?>
+                    <div class="number count-to" data-from="0" data-to="<?php echo $row2->jumlah2; ?>" data-speed="1000" data-fresh-interval="20"><?php echo $row2->jumlah2; ?></div>
+						<?php } ?>
                 </div>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
             <div class="info-box bg-light-green hover-expand-effect">
                 <div class="icon">
-                    <i class="material-icons">forum</i>
+                    <i class="material-icons">person</i>
                 </div>
                 <div class="content">
-                    <div class="text">NEW COMMENTS</div>
-                    <div class="number count-to" data-from="0" data-to="243" data-speed="1000" data-fresh-interval="20">243</div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box bg-orange hover-expand-effect">
-                <div class="icon">
-                    <i class="material-icons">person_add</i>
-                </div>
-                <div class="content">
-                    <div class="text">NEW VISITORS</div>
-                    <div class="number count-to" data-from="0" data-to="1225" data-speed="1000" data-fresh-interval="20">1225</div>
+					<?php
+						$cek3 = $this->db->query('Select count(id_pengguna) AS jumlah3 from tbl_pengguna where level="3" ');
+						foreach ($cek3->result() as $row3) {
+								?>                    
+					<div class="text">PENGGUNA</div>
+                    <div class="number count-to" data-from="0" data-to="<?php echo $row3->jumlah3; ?>" data-speed="1000" data-fresh-interval="20"><?php echo $row3->jumlah3; ?></div>
+						<?php } ?>
                 </div>
             </div>
         </div>
