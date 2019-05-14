967d4e074c5dee4ab5b4aff3bcf1b5e9dceb756b
yesikatr <trestiarso@gmail.com>
2019-05-14 14:05:21 +0700

    shadow form

--- application/views/posko/form_timeline.php
+++ application/views/posko/form_timeline.php
@@ -4,7 +4,7 @@
                  <!-- Horizontal Layout -->
             <div class="row clearfix">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="card">
+                    <div class="card no-shadow">
                         <div class="header">
                             <h2>
                                 TAMBAH TIMELINE
